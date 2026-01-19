9353a3118810b56df09337ece31e221d308ae64d
Bhumika Jawale <bhumikajawale@Bhumikas-MacBook-Air.local>
2026-01-19 19:36:44 +0530

    Handwritten Character Recognition project completed

--- predict.py
+++ predict.py
@@ -2,15 +2,22 @@ import cv2
 import numpy as np
 from tensorflow.keras.models import load_model
 
-model = load_model("../models/handwritten_cnn.h5")
+model = load_model("models/handwritten_cnn.h5")
 
 def predict_image(image_path):
+
     img = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+
     img = cv2.resize(img, (28,28))
-    img = img.astype('float32')/255.0
+
+    img = cv2.bitwise_not(img)
+
+    img = img.astype("float32") / 255.0
+
     img = img.reshape(1,28,28,1)
 
     pred = model.predict(img)
     print("Predicted Digit:", np.argmax(pred))
 
-predict_image("test_digit.png")
+predict_image("1.jpeg")
+
